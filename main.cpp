8a588ed6acb264dff966b949459f1d7504c3cdfa
DrowsyTM <dima.gamolya@gmail.com>
2024-05-28 22:04:34 -0700

    Moved ranGen to global Implemented ProfileFunctions WIP Redo of LocalFeatureGen

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream> //Files
 #include <chrono> //Time
 #include <thread> //Create concurrent threads
 #include <mutex> //Give thread exclusive access to a variable/object
@@ -22,21 +23,63 @@ terrain worldMap[LMAP][LMAP]; //2D enum array of size LMAP - should be much larg
 // Maybe create custom class for it to create custom behavior
 // Also iteration would be nice and simplifies code
 
+default_random_engine ranGen(chrono::system_clock::now().time_since_epoch().count());
+
+bool debug = true;
+
 int GameTick();
 
 void LocalMapGen(); //Generates local map
-void LocalTerraGen(auto d100);  // Generates terrain
-void LocalFeatureGen(auto d100); // Generates features (forest)
+void LocalTerraGen();  // Generates terrain
+void LocalFeatureGen(); // Generates features (forest)
 
 void Draw();
 
+void ProfileFunctions() {
+    cout << "Profile of all Functions\n\n";
+    const int iterations = 1000;
+    double time = 0; //ms
+
+    // Profile LocalMapGen
+    auto start = high_resolution_clock::now();
+    for (int i = 0; i < iterations; ++i) {
+        LocalMapGen();
+    }
+    auto stop = high_resolution_clock::now();
+    time = static_cast<double>(duration_cast<microseconds>(stop - start).count()) / 1000;
+    cout << "Average LocalMapGen Time: \t" << time / iterations << " ms\n";
+
+    // Profile LocalTerraGen
+    start = high_resolution_clock::now();
+    for (int i = 0; i < iterations; ++i) {
+        LocalTerraGen();
+    }
+    stop = high_resolution_clock::now();
+    time = static_cast<double>(duration_cast<microseconds>(stop - start).count()) / 1000;
+    cout << "Average LocalTerraGen Time: \t" << time / iterations << " ms\n";
+
+    // Profile LocalFeatureGen
+    start = high_resolution_clock::now();
+    for (int i = 0; i < iterations; ++i) {
+        LocalFeatureGen();
+    }
+    stop = high_resolution_clock::now();
+    time = static_cast<double>(duration_cast<microseconds>(stop - start).count()) / 1000;
+    cout << "Average LocalFeatureGen Time: \t" << time / iterations << " ms\n";
+}
+
 int main() {
-    jthread mapGen(LocalMapGen); // Start generating local map
+
+    if (debug) {
+        jthread profileThread(ProfileFunctions);
+    }
+
+    jthread mapThread(LocalMapGen); // Start generating local map
     cout << "Generating Local Map...\n";
 
-    mapGen.join(); // Wait for map to finish - wil remove this and the print and go into menu
+    mapThread.join(); // Wait for map to finish - wil remove this and the print and go into menu
 
-    Draw();
+//    Draw();
 
     cin.ignore(); // Wait for user input before ending
     return 0;
@@ -60,30 +103,31 @@ int GameTick() {
 
 // Pre-generates local play area (reduce loading time & load rest of map later)
 void LocalMapGen() {
-    default_random_engine ranGen;
-    uniform_int_distribution<int> distribution(1, 100);
-    auto d100 = bind(distribution, ranGen); //bind(func, args...)
+
     //int result = distribution(ranGen);
 
-    LocalTerraGen(d100);
-    LocalFeatureGen(d100);
-    //Figure out how to make all this work for GlobalMap. Maybe a "chunk" system? Rolls per chunk for feature?
-    //Rolls per chunk means we can just roll for the chunk then randomly place it
-    //Or maybe just no chunks and some sort of probability system that keeps in track how close you are
-    //To another forest. Low chance to be really close, becomes more likely farther away, more consistent
-    //Or chunks + probability distance. Dunno, not sure why I use chunks(usually for 3d optimization?)
-    //Though chunks would be about unloading chunks. We want to update whole world.
-    //Or maybe chunks work with the retroactive updates to AI and world updates?
+    LocalTerraGen();
+    LocalFeatureGen();
 
-    //Maybe one WorldMapGen that simply stops
 
-    //For map rather than iterating every single coordinate, I probably want to
-    //Transform noise/map into what I need. With matrix stuff? Or is this more calculations. Large noise map?
-
-    //Ahh, world gen should probably run on multiple threads to accelerate it
 }
 
-void LocalTerraGen(auto d100) {
+//Figure out how to make all this work for GlobalMap. Maybe a "chunk" system? Rolls per chunk for feature?
+//Rolls per chunk means we can just roll for the chunk then randomly place it
+//Or maybe just no chunks and some sort of probability system that keeps in track how close you are
+//To another forest. Low chance to be really close, becomes more likely farther away, more consistent
+//Or chunks + probability distance. Dunno, not sure why I use chunks(usually for 3d optimization?)
+//Though chunks would be about unloading chunks. We want to update whole world.
+//Or maybe chunks work with the retroactive updates to AI and world updates?
+
+//Maybe one WorldMapGen that simply stops
+
+//For map rather than iterating every single coordinate, I probably want to
+//Transform noise/map into what I need. With matrix stuff? Or is this more calculations. Large noise map?
+
+//Ahh, world gen should probably run on multiple threads to accelerate it
+
+void LocalTerraGen() {
     // Iterate through Local Map Size
     int temp;
 
@@ -94,33 +138,38 @@ void LocalTerraGen(auto d100) {
             }
         }
     }
+
 }
 
-void LocalFeatureGen(auto d100) {
+void LocalFeatureGen() {
     //For features, rather than iterate, we can just aim for a random spot since we don't fill everything
 
+    uniform_int_distribution<int> dist32(0, LMAP-1);
+    auto d32 = bind(dist32, ranGen);
+
+    uniform_int_distribution<int> dist1000(1, 1000);
+    auto d1000 = bind(dist1000,ranGen);
+
+
     //Forest chance 1/500
-    int mapSize = LMAP*LMAP;
+    int mapSize = LMAP * LMAP;
     int rolls = mapSize / 500 + 1; //Max forests
-    cout << "Num Forest Rolls: " << rolls << "\n";
+    //cout << "Num Forest Rolls: " << rolls << "\n";
 
     //Iterate through whole thing for now, annoying, implement some dice globally?
     for (int y = 0; y < LMAP; y++) {
         for (int x = 0; x < LMAP; x++) {
             if (rolls > 0) {
-                if (d100() == 1) { // Ok this is 1% so this is gonna fill up top map
+                if (d1000() <= 2) { // 1/500 | 0.2%
                     worldMap[y][x] = FOREST;
                     // do genForest() function instead
                     rolls--;
                 }
-            }
-            else {
+            } else {
                 break;
             }
         }
     }
-
-
 }
 
 void Draw() {
@@ -136,7 +185,6 @@ void Draw() {
 
 
 /** TODO
- GITHUB
  MEASURE EXECUTION TIME OF EVERYTHING.
  LOGS? DEBUG MODE? Custom log message at the start of every one like (Change Terrain to Noise)
  Work on map generator, forest gen
