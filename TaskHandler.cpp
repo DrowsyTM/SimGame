0ee1855630b58d8b12ce4b0958bbe83f339ef87f
DrowsyTM <dima.gamolya@gmail.com>
2024-05-31 03:14:20 -0700

    WIP full rewrite of TaskHandler using loaders. Interesting idea

--- TaskHandler.cpp
+++ TaskHandler.cpp
@@ -1,89 +1,118 @@
 #include "TaskHandler.h"
 
-TaskHandler::TaskHandler()
-        : tasks{}, numThreads{1}, cv{}, mtx{}, stopSource{},
-          stopToken{stopSource.get_token()} {
-    for (int i = 0; i < numThreads; i++) {
-        workers.emplace_back([this]() { this->workerThread(); });
+//Delegates to full constructor
+TaskHandler::TaskHandler() : TaskHandler(10, 10, 10, false) {}
+
+//Delegates to full constructor
+TaskHandler::TaskHandler(int workerCount, int loaderCount, int bucketSize)
+        : TaskHandler(workerCount, loaderCount, bucketSize, false) {}
+
+//Full Constructor
+TaskHandler::TaskHandler(int workerCount, int loaderCount, int bucketSize, bool doLogging)
+        : num_workers{workerCount}, num_loaders{loaderCount}, bucket_size(bucketSize), logger_flag{doLogging},
+          stop_flag{false} {
+
+
+    work_arrays.resize(workerCount);
+    for (int i = 0; i < workerCount; i++) {
+        work_arrays[i].resize(10); //
     }
-}
 
-TaskHandler::~TaskHandler() {
-
-    stopSource.request_stop();
-    cv.notify_all();
-
-    for (auto &worker: workers) {
-        if (worker.joinable()) {
-            worker.join();
-        } else {
-            std::cout << "Worker didn't join\n";
-        }
-    }
-}
-
-int TaskHandler::numTasks() {
-    return tasks.size();
-}
-
-void TaskHandler::waitOnEmpty() {
-    while (!tasks.empty()) {
-    }
-    return;
-}
-
-//---------------Private-------------------
-
-//Waits on queue, executes tasks from queue
-void TaskHandler::workerThread() {
-    while (true) {//I think this should be true instead
-        std::unique_ptr<Task> task;
-        {
-            std::unique_lock<std::mutex> lock(mtx);
-            cv.wait(lock, [this] { return !tasks.empty() || stopToken.stop_requested(); });
-            if (stopToken.stop_requested() && tasks.empty()) {
-                return;
-            }
-            if (!tasks.empty()) {
-                task = std::move(tasks.front());
-                tasks.pop();
-            }
-        }
-        if (task) {
-            try { //If execution fails for some reason, catches exception and prints it.
-                task->execute();
-            } catch (const std::exception &e) {
-                std::cout << e.what();
-            }
-        }
+    for (int i = 0; i < num_workers; i++) {
+        workers.emplace_back([this]() { /**work function with ID**/ });
     }
 }
 
 
-//Safely pushes onto queue
-void TaskHandler::push(std::unique_ptr<Task> task) {
-    std::lock_guard<std::mutex> lock(mtx);
-    {
-        tasks.push(std::move(task));
+void TaskHandler::LoadingBay(WorldMap &map) {
+    for (int i = 0; i < num_workers; i++) {
+        loaders.emplace_back([this, &map, &i]() { loadMapTasks(map, i); });
     }
-    cv.notify_one(); //lets a thread through?
 }
 
+void TaskHandler::loadMapTasks(WorldMap &map, int ID) {
+    int amount = map.numChunks() / num_loaders;
+    int start = amount * ID;
 
-//---------------Work with External Objects-------------------
-void TaskHandler::taskMapGeneration(WorldMap &map) {
-    int num_chunks = map.numChunks();
-    //Prevents multiple threads from doing this, but I'm not sure it matters
-    for (int i = 0; i < num_chunks; i++) {
-        push(std::make_unique<TaskTerrainGen>(i, &map));
-    }
-    std::cout << "Generated " << num_chunks << " chunks.\n";
+
 }
 
-
-
-
-
-
-// Cleaner process should be its own task with .execute() ? Maybe
-// No cleaner, just blacklist index and replace it?
+//TaskHandler::~TaskHandler() {
+//
+//    stopSource.request_stop();
+//    cv.notify_all();
+//
+//    for (auto &worker: workers) {
+//        if (worker.joinable()) {
+//            worker.join();
+//        } else {
+//            std::cout << "Worker didn't join\n";
+//        }
+//    }
+//}
+//
+//int TaskHandler::numTasks() {
+//    return tasks.size();
+//}
+//
+//void TaskHandler::waitOnEmpty() {
+//    while (!tasks.empty()) {
+//    }
+//    return;
+//}
+//
+////---------------Private-------------------
+//
+////Waits on queue, executes tasks from queue
+//void TaskHandler::workerThread() {
+//    while (true) {//I think this should be true instead
+//        std::unique_ptr<Task> task;
+//        {
+//            std::unique_lock<std::mutex> lock(mtx);
+//            cv.wait(lock, [this] { return !tasks.empty() || stopToken.stop_requested(); });
+//            if (stopToken.stop_requested() && tasks.empty()) {
+//                return;
+//            }
+//            if (!tasks.empty()) {
+//                task = std::move(tasks.front());
+//                tasks.pop();
+//            }
+//        }
+//        if (task) {
+//            try { //If execution fails for some reason, catches exception and prints it.
+//                task->execute();
+//            } catch (const std::exception &e) {
+//                std::cout << e.what();
+//            }
+//        }
+//    }
+//}
+//
+//
+////Safely pushes onto queue
+//void TaskHandler::push(std::unique_ptr<Task> task) {
+//    std::lock_guard<std::mutex> lock(mtx);
+//    {
+//        tasks.push(std::move(task));
+//    }
+//    cv.notify_one(); //lets a thread through?
+//}
+//
+//
+////---------------Work with External Objects-------------------
+//void TaskHandler::taskMapGeneration(WorldMap &map) {
+//    int num_chunks = map.numChunks();
+//    //Prevents multiple threads from doing this, but I'm not sure it matters
+//    for (int i = 0; i < num_chunks; i++) {
+//        push(std::make_unique<TaskTerrainGen>(i, &map));
+//    }
+//    std::cout << "Generated " << num_chunks << " chunks.\n";
+//}
+//
+//
+//
+//
+//
+//
+//// Cleaner process should be its own task with .execute() ? Maybe
+//// No cleaner, just blacklist index and replace it?
--- TaskHandler.h
+++ TaskHandler.h
@@ -2,36 +2,63 @@
 #define SIMGAME_TASKHANDLER_H
 
 #include <vector>
+#include <array>
 #include "WorldMap.h"
 #include "Task.h"
 #include <atomic>
 #include <condition_variable>
 #include <queue>
 #include <memory>
+#include <fstream>
 
 class TaskHandler {
 public:
     TaskHandler();
 
+    TaskHandler(int workerCount, int loaderCount, const int bucketSize);
+
+    TaskHandler(int workerCount, int loaderCount, const int bucketSize, bool doLogging);
+
     ~TaskHandler();
 
-    void taskMapGeneration(WorldMap& map);
-    int numTasks();
-    void waitOnEmpty();
+    void LoadingBay(WorldMap &map);
+//
+//    void taskMapGeneration(WorldMap& map);
+//    int numTasks();
+//    void waitOnEmpty();
 
 private:
     std::vector<std::jthread> workers;
-    std::queue<std::unique_ptr<Task>> tasks;
-    int numThreads;
-    std::mutex mtx;
-    std::mutex worker_mtx;
-    std::stop_source stopSource;
-    std::stop_token stopToken; //Whether threads should stop permanently
-    std::condition_variable cv; //Signals whether tasks are available
+    std::vector<std::jthread> loaders;
 
-    void workerThread();
 
-    void push(std::unique_ptr<Task> task);
+    //Wait why am I using a unique ptr for Task? Hm prob so that it can be passed to me.
+    // Honestly Task is smaller than a ptr and there are so many more things to be optimized.
+    // But I dunno.
+    std::vector<std::vector<std::unique_ptr<Task>>> work_arrays;
+    std::vector<std::vector<std::unique_ptr<Task>>> load_arrays;
+
+    int num_workers;
+    int num_loaders;
+    int bucket_size;
+    bool stop_flag;
+    bool logger_flag;
+
+    void loadMapTasks(WorldMap &map, int ID);
+
+//    std::queue<std::unique_ptr<Task>> tasks;
+
+//    std::mutex mtx;
+//    std::mutex worker_mtx;
+//    std::stop_source stopSource;
+//    std::stop_token stopToken; //Whether threads should stop permanently
+//    std::condition_variable cv; //Signals whether tasks are available
+//
+//
+//
+//    void workerThread();
+//
+//    void push(std::unique_ptr<Task> task);
 
 };
 
