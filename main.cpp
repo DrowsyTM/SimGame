bc09291d487e1065ca0e8e48d9eccaa41587e68c
DrowsyTM <dima.gamolya@gmail.com>
2024-05-30 20:28:55 -0700

    Cleaned up main

--- main.cpp
+++ main.cpp
@@ -10,19 +10,13 @@
 #include <format>
 
 #include "WorldMap.h"
+#include "TaskHandler.h"
 
 const int MAP_DIMENSION_UPPER_LIMIT = 15000;
 
 using namespace std;
 using namespace std::chrono_literals;
 using namespace std::chrono;
-
-const int LMAP = 32; // Size of initial map
-//Temporarily global, will change to reference when I get to it
-terrain worldMap[LMAP][LMAP]; //2D enum array of size LMAP - should be much larger than LMAP
-// Maybe create custom class for it to create custom behavior
-// Also iteration would be nice and simplifies code
-default_random_engine randomGenerator(system_clock::now().time_since_epoch().count());
 
 struct ForestNode {
     int x, y;
@@ -33,13 +27,7 @@ int forestAmount = 0;
 
 int GameTick();
 
-void LocalFeatureGen(); // Generates features (forest)
-void GenerateForest(const int x, const int y);
-
-void MergeFeatures();
-
-void Draw();
-
+/**
 void ProfileFunctions() {
 
     const int iterations = 1000;
@@ -108,22 +96,16 @@ void ProfileFunctions() {
     logFile << "\n";
     logFile.close();
 }
+**/
 
 int main() {
-//    void (*limitedMapGenerator)(int, int) = MapGenerator; //function pointer to overload
-//    jthread mapThread(limitedMapGenerator, 32, 500); // Start generating local map
-//    jthread mapThread([]() { MapGenerator(32, 500); }); //Lambda func
+    //    jthread mapThread([]() { MapGenerator(32, 500); }); //Lambda func
+    //Hmm just implement TPS and use that to measure changes?
 
-//Hmm just implement TPS and use that to measure changes?
-
-    WorldMap test;
-    cout << test.numChunks();
-
-    if (false) {
-        jthread profileThread(ProfileFunctions);
-    }
-
-    // menu();
+    WorldMap map;
+    TaskHandler tasker;
+    tasker.taskMapGeneration(map);
+    map.draw();
 
     cin.ignore(); // Wait for user input before ending
     return 0;
@@ -138,49 +120,7 @@ int GameTick() {
     return 0;
 }
 
-//For forests, either remove them from terrain gen or spawn them super rare with a custom terrain symbol
-// Then go around and place forest clumps at those terrain synmbols
-// Or change how worldGen works. First generate ground, then "ecosystems", then "stuctures", in a systematic way
-// This would also allow layers. I can have a ground layer, then layers for other stuff.
-// How to display tho?? Feels like 3D is just has a lot more potential, or at least 3D on 2D plane?
-
-// Pre-generates local play area (reduce loading time & load rest of map later)
-// Need to start near middle of map or something. Or random spot
-
-//Figure out how to make all this work for GlobalMap. Maybe a "chunk" system? Rolls per chunk for feature?
-//Rolls per chunk means we can just roll for the chunk then randomly place it
-//Or maybe just no chunks and some sort of probability system that keeps in track how close you are
-//To another forest. Low chance to be really close, becomes more likely farther away, more consistent
-//Or chunks + probability distance. Dunno, not sure why I use chunks(usually for 3d optimization?)
-//Though chunks would be about unloading chunks. We want to update whole world.
-//Or maybe chunks work with the retroactive updates to AI and world updates?
-
-//Maybe one WorldMapGen that simply stops
-
-//For map rather than iterating every single coordinate, I probably want to
-//Transform noise/map into what I need. With matrix stuff? Or is this more calculations. Large noise map?
-
-//Ahh, world gen should probably run on multiple threads to accelerate it
-
-void LocalFeatureGen() {
-    //For features, rather than iterate, we can just aim for a random spot since we don't fill everything
-
-    static uniform_int_distribution<int> dist32(0, LMAP - 1);
-    static auto d32 = bind(dist32, randomGenerator);
-
-
-    //Forest chance 1/500
-    int mapSize = LMAP * LMAP;//Max forests
-    int rolls = mapSize / 500 + 1;
-    //cout << "Num Forest Rolls: " << rolls << "\n";
-
-    //Iterate through whole thing for now, annoying, implement some dice globally?
-
-    for (; rolls > 0; rolls--) {
-        GenerateForest(d32(), d32());
-    }
-}
-
+/**
 void GenerateForest(const int x, const int y) {
     static uniform_int_distribution<int> dist02(-1, 1);
     static auto d02 = bind(dist02, randomGenerator);
@@ -209,24 +149,31 @@ void GenerateForest(const int x, const int y) {
     // Node gets its own map. Function that takes this map and puts it onto world?
 
 }
+**/
 
-void MergeFeatures() {
-    //Just write local map -> world map translation function?
-    //Maybe make it part of Forest function
-    for (int i = 0; i < forestAmount; i++) {
-        ForestNode node = forestList[i];
-        for (int y = 0; y < 15; y++) {
+//For forests, either remove them from terrain gen or spawn them super rare with a custom terrain symbol
+// Then go around and place forest clumps at those terrain synmbols
+// Or change how worldGen works. First generate ground, then "ecosystems", then "stuctures", in a systematic way
+// This would also allow layers. I can have a ground layer, then layers for other stuff.
+// How to display tho?? Feels like 3D is just has a lot more potential, or at least 3D on 2D plane?
 
-            bool validY = y + node.y - 7 >= 0 && y + node.y - 7 < LMAP;
-            for (int x = 0; x < 15; x++) {
-                bool validX = node.map[y][x] == true && x + node.x - 7 >= 0 && x + node.x - 7 < LMAP;
-                if (validX && validY) {
-                    worldMap[y + node.y - 7][x + node.x - 7] = FOREST;
-                }
-            }
-        }
-    }
-}
+// Pre-generates local play area (reduce loading time & load rest of map later)
+// Need to start near middle of map or something. Or random spot
+
+//Figure out how to make all this work for GlobalMap. Maybe a "chunk" system? Rolls per chunk for feature?
+//Rolls per chunk means we can just roll for the chunk then randomly place it
+//Or maybe just no chunks and some sort of probability system that keeps in track how close you are
+//To another forest. Low chance to be really close, becomes more likely farther away, more consistent
+//Or chunks + probability distance. Dunno, not sure why I use chunks(usually for 3d optimization?)
+//Though chunks would be about unloading chunks. We want to update whole world.
+//Or maybe chunks work with the retroactive updates to AI and world updates?
+
+//Maybe one WorldMapGen that simply stops
+
+//For map rather than iterating every single coordinate, I probably want to
+//Transform noise/map into what I need. With matrix stuff? Or is this more calculations. Large noise map?
+
+//Ahh, world gen should probably run on multiple threads to accelerate it
 
 
 
